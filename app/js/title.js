7e083a47ac47bcb51fa7fd3df161e34524205acf
Yud1nArtem <yudin20011610@gmail.com>
2021-04-03 23:36:47 +0300

    do header

--- app/js/title.js
+++ app/js/title.js
@@ -1,1 +1,19 @@
-alert(123)
+let i = 1;
+let arr = [];
+function addSpan () {
+    let a = 10;
+    if (i <= 9) {
+        let val = document.querySelector('span:nth-child(' + i +')');
+        arr.push(val);
+        val.classList.add('active');
+        setTimeout(addSpan, 500, ++i);
+    }
+    else if (a > 9) {
+        let val = document.querySelector('span:nth-child(' + a +')');
+        arr.pop(val);
+        val.classList.remove('active');
+        setTimeout(changeSpan, 500, --a);
+    }
+}
+
+setTimeout(addSpan, 500);
